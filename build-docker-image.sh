0a2a852d4301af7cfdb6e0468d91d1a6c5eb7265
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2015-11-16 13:16:50 +0100

    Building a docker image

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
+function usage {
+    echo "usage:"
+    echo "$script_name \$jar_file"
+    exit 1
+}
+trap usage ERR
 set -e
+
 function main {
     init "$@"
-    build-docker-image
+    build_docker_image
 }
 
 function init {
     script_name=`basename $0`
-    script_root=`dirname $(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0)`
     if [ -z "$1" ]
     then
-        project_root=`pwd`
-    else
-        project_root=$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1)
-        if [ ! -d "$project_root" ]
-        then
-            echo "Argument must be a directory"
-            usage
-        fi
+        echo "You must provide a jar file"
+        exit 1
     fi
-    dockerfile_template=project
+    if [ ! -f "$1" ]
+    then
+        echo "You must provide a jar file argument was `file $1`"
+        exit 1
+    fi
+    jar_file=$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1)
+
+    has_main_class=`unzip -q -c $jar_file META-INF/MANIFEST.MF|grep Main-Class`
+
+    if [ -z "$has_main_class" ]
+    then
+        echo "Illegal jar file"
+        exit 1
+    fi
+
+    script_root=`dirname $(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0)`
+    project_root=/tmp/docker-$$.$RANDOM
 }
 
-function usage {
-    echo "$script_name <project_dir>"
-    exit 1
+function build_docker_image {
+    mkdir $project_root
+    cp $jar_file $project_root
+    cp $script_root/scripts/start-app.sh $project_root
+    export JAR_FILE=`basename $jar_file`
+    render_template $script_root/templates/Dockerfile > $project_root/Dockerfile
+    cd $project_root
+    docker build -t `echo $JAR_FILE|tr '[:upper:]' '[:lower:]'` .
 }
 
-function build-docker-image {
-    echo $script_root
-    echo $project_root
+function render_template {
+    eval "echo \"$(cat $1)\""
 }
 main "$@"
---
+++ scripts/start-app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Starting app"
+java -jar ejendom-1.0.0-standalone.jar 8000
