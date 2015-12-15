8c7a5b05b4cd022d1cec80df8a19331f2dc3cb0a
Go <go@skm.dk>
2015-12-15 09:14:45 +0100

    Add docker version to app version

--- go-build-image.sh
+++ go-build-image.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build -t $APP_NAME:$BUILD_NO .
+app_version=$APP_NAME:$BUILD_NO.$GO_PIPELINE_COUNTER
+docker build -t $app_version .
+docker tag -f $app_version 172.21.14.12:5000/$app_version
+docker push 172.21.14.12:5000/$app_version
